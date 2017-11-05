edb21e3d7f43917e1fc436f00552991574ab5eeb
Ollisteka <zhukova.o.m@yandex.ru>
2017-11-05 21:07:00 +0500

    Циферки внутри подчёркивания не обрабатываются

--- Markdown/SingleUnderscore.cs
+++ Markdown/SingleUnderscore.cs
@@ -40,19 +40,28 @@ namespace Markdown
 					opening = !opening;
 					continue;
 				}
+				// check for screened underscores
+				if (i >= 1 && markdown[i - 1] == '\\')
+				{
+					screens.Add(i - 1);
+					//opening = !opening;
+					continue;
+				}
 				// check for space after opening underscore
 				if (opening && i + 1 < markdown.Length && markdown[i + 1] == ' ')
 					continue;
 				// check for space before closing iunderscore
 				if (!opening && markdown[i - 1] == ' ')
 					continue;
-				// check for screened underscores
-				if (i >= 1 && markdown[i - 1] == '\\')
+				// check for numbers inside
+				if (!opening)
 				{
-					entries.Remove(i);
-					screens.Add(i - 1);
-					opening = !opening;
-					continue;
+					var lastElement = entries.Keys.Max();
+					if (markdown.Substring(lastElement, i - lastElement).Any(char.IsDigit))
+					{
+						entries.Remove(lastElement);
+						opening = true;
+					}
 				}
 				entries.Add(i, opening);
 				opening = !opening;
@@ -65,6 +74,11 @@ namespace Markdown
 		{
 			var result = new StringBuilder(markdown);
 			var offset = 0;
+			foreach (var position in screens)
+			{
+				result.Remove(position + offset, 1);
+				offset -= 1;
+			}
 			foreach (var entry in Entries)
 			{
 				result.Remove(entry.Key + offset, 1);
@@ -72,11 +86,6 @@ namespace Markdown
 				if (entry.Value)
 					offset += 3;
 				else offset += 4;
-			}
-			foreach (var position in screens)
-			{
-				result.Remove(position + offset, 1);
-				offset -= 1;
 			}
 			return result.ToString();
 		}
